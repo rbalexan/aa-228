07b0e7cfbcad473f14d26f88d5380e0efbf6e289
rbalexan <rbalexan@stanford.edu>
2019-10-17 23:27:52 -0700

    implement K2 search

---
+++ project-1/multiK2Search.jl
@@ -0,0 +1,65 @@
+using LightGraphs
+using DataFrames
+using GraphPlot
+using Compose
+using SpecialFunctions
+using Random
+using CSV
+using Printf
+using Colors
+
+include("bayesianScore.jl")
+include("bayesianCounts.jl")
+include("initializeExampleGraph.jl")
+include("initializeUnconnectedGraph.jl")
+include("singleK2Search.jl")
+include("writeGraph.jl")
+
+iterations  = parse(Int64, ARGS[1])
+infile      = ARGS[2]
+outfile     = ARGS[3]
+
+Random.seed!(1)
+
+function multiK2Search(dataset, iterations)
+
+    bestScore = -1E20
+    bestGraph = DiGraph(0)
+
+    for iteration in 1:iterations
+
+        @show iteration
+
+        graph, score = singleK2Search(dataset)
+
+        if score > bestScore
+            bestScore = score
+            bestGraph = graph
+        end
+
+        @show bestScore
+
+    end
+
+    return bestGraph, bestScore
+
+end
+
+# load the dataset
+dataset = CSV.read("data/" * infile * ".csv")
+
+(bestGraph, bestScore), time = @timed multiK2Search(dataset, iterations)
+@show time
+@show bestScore
+
+# output the .gph and .svg files
+println("printing graph")
+nodes = Dict(i => names(dataset)[i] for i in 1:size(dataset)[2])
+writeGraph(bestGraph, nodes, "graphs/" * outfile * ".gph")
+draw(SVG("graphs/" * outfile * ".svg", 16cm, 16cm), gplot(bestGraph,
+    nodelabel=names(dataset), nodefillc=[RGB(0.10, 0.58, 0.86) for i in 1:size(dataset)[2]]))
+open("graphs/" * outfile * ".dat", "w") do io
+    @printf(io, "Best Score | %f \n", bestScore)
+    @printf(io, "Time       | %f \n", time)
+    @printf(io, "Iterations | %.0f", iterations)
+end
---
+++ project-1/singleK2Search.jl
@@ -0,0 +1,68 @@
+# K2 graph search algorithm (single iteration)
+function singleK2Search(dataset)
+
+    # initialize completely unconnected graph and score it
+    graph       = initializeUnconnectedGraph(size(dataset)[2])
+    score       = bayesianScore(bayesianCounts(dataset, graph)...)
+
+    bestGraph   = deepcopy(graph)
+    bestScore   = deepcopy(score)
+    #@show bestScore
+
+    # generate ordering of nodes
+    nodeOrdering = randperm(size(dataset)[2]) # can remove the rng later
+    @show nodeOrdering
+
+    for i in nodeOrdering
+
+        @show i
+
+        # generate the set of possible parents for the node
+        validParents = filter(x->x≠i, nodeOrdering)
+
+        while length(inneighbors(graph, i)) < min(length(nodeOrdering) - 1, 10) # max parents
+
+            # re/initialize possible graph scores
+            graphScores = [-1.0E20 for i in 1:size(dataset)[2]]
+
+            # remove new invalid graphs & compute the Bayesian score for all new valid graphs
+            for j in deepcopy(validParents)
+
+                add_edge!(graph, j, i)
+                if is_cyclic(graph)
+                    filter!(x->x≠j, validParents)
+                else
+                    graphScores[j] = bayesianScore(bayesianCounts(dataset, graph)...)
+                end
+                rem_edge!(graph, j, i)
+
+            end
+
+            # if we have a better graph, build, score, and save the best graph
+            if maximum(graphScores) > bestScore
+
+                bestParent = argmax(graphScores)
+                add_edge!(graph, bestParent, i)
+
+                bestGraph = deepcopy(graph)
+                bestScore = maximum(graphScores)
+
+                # remove best parent from list of possible parents
+                filter!(x->x≠bestParent, validParents)
+
+            else
+
+                # no new graph yields an improvement
+                break
+
+            end
+
+        end
+
+        #@show bestScore
+
+    end
+
+    return bestGraph, bestScore
+
+end
