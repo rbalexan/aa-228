174a1b28cb7205ec3f754c53b28aa9eea4893b5e
rbalexan <rbalexan@stanford.edu>
2019-11-08 19:00:32 -0800

    create modalPolicyFilling.jl

---
+++ project-2/modalPolicyFilling.jl
@@ -0,0 +1,24 @@
+function modalPolicyFilling(𝖲, U, π, N)
+
+    πp = deepcopy(π)
+
+    for s in 1:𝖲
+
+        if U[s] == 0
+
+            # return the neighbors of s where we have a nonzero U value
+            neighborhood   = N(s, 1)
+            validNeighbors = findall(U[neighborhood] .!= 0)
+
+            # compute the policy mode for the valid neighbors
+            if !isempty(validNeighbors)
+                πp[s] = max(0, mode(π[neighborhood[validNeighbors]]))
+            end
+
+        end
+
+    end
+
+    return πp
+
+end
